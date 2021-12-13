ae35164be0bd77bbcb211d732e09948afeea216b
shuaibin <3166688833@qq.com>
2021-12-13 11:05:19 +0800

    init

---
+++ spring-web/src/test/java/cf/binzikeji/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.binzikeji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
